443835563f2c754ee1234c05d4571a4ff19bf899
Adam Gray <adam@pateo.co>
2017-09-05 20:36:30 +0100

    Show other regions

--- src/components/RegionList.js
+++ src/components/RegionList.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import RegionButton from './RegionButton'
 import DemoCard from './DemoCard'
 
-const RegionList = () => (
-  <div>
-    <DemoCard />
-  </div>
-)
+const RegionList = ({ title, demos }) => {
+  return (
+    <div>
+      <RegionButton title={title} />
+      {demos.filter(demo => demo.region === title)[0].cards.map((demo) => (
+        <DemoCard key={demo.id}{...demo} />
+      ))}
+    </div>
+  )
+}
 
 export default RegionList
--- src/containers/RegionsContainer.js
+++ src/containers/RegionsContainer.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import RegionContainer from './RegionContainer'
+import RegionList from '../components/RegionList'
 
-const RegionsContainer = ({ regions }) => (
+const RegionsContainer = ({ regions, demos }) => (
   <div>
     <h2>Regions</h2>
     {regions.map((region, index) => (
-      <RegionContainer key={index} title={region} />
+      <RegionList key={index} title={region} demos={demos} />
     ))}
   </div>
 )
 
 const mapStateToProps = (state) => ({
-  regions: state.demos.map(demo => demo.region)
+  regions: state.demos.filter(demo => demo.region !== state.currentRegion).map(demo => demo.region),
+  demos: state.demos
 })
 
 export default connect(mapStateToProps)(RegionsContainer)
