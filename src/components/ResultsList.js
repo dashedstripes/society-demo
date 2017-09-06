11a0c70df20761e8aa2a1be618094901837ba2ab
Adam Gray <adam@pateo.co>
2017-09-06 14:37:38 +0100

    Show msg when no results

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -3,17 +3,23 @@ import DemoCard from './DemoCard'
 
 class ResultsList extends Component {
   render() {
-    return (
-      <div className="row">
-        {this.props.results.map((result) => {
-          return (
-            <div className="col-md-4">
-              <DemoCard key={result.id} {...result} />
-            </div>
-          )
-        })}
-      </div>
-    )
+    if (this.props.results.length > 0) {
+      return (
+        <div className="row">
+          {this.props.results.map((result) => {
+            return (
+              <div key={result.id} className="col-md-4">
+                <DemoCard {...result} />
+              </div>
+            )
+          })}
+        </div>
+      )
+    } else {
+      return (
+        <h4>No results found...</h4>
+      )
+    }
   }
 }
 
