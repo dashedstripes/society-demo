73b1d876c3c0298703b32ea7d014308cc52fc559
Adam Gray <adam@pateo.co>
2017-09-06 15:15:25 +0100

    Stop subscribing to store as there are redux devtools

--- src/index.js
+++ src/index.js
@@ -104,10 +104,6 @@ const store = createStore(reducer, {
   ]
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-store.subscribe(() => {
-  console.log(store.getState())
-})
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
