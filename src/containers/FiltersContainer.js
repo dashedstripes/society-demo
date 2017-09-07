5c3daf4649d144c570779a01fe8594f3228d6364
Adam Gray <adam@pateo.co>
2017-09-07 10:31:51 +0100

    Fix filter button width

--- src/containers/FiltersContainer.js
+++ src/containers/FiltersContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { toggleFilters } from '../actions'
 import FiltersButton from '../components/FiltersButton'
 import FiltersList from '../components/FiltersList'
+import '../styles/FiltersContainer.css'
 
 class FiltersContainer extends Component {
   handleChange(isOpen) {
@@ -12,7 +13,7 @@ class FiltersContainer extends Component {
     if (this.props.filters.isOpen) {
       return (
         <div className="row">
-          <div className="col-md-12">
+          <div className="col-md-12 FiltersContainerColumn">
             <FiltersButton handleChange={this.handleChange.bind(this, false)} />
             <FiltersList options={this.props.filters.options} />
           </div>
@@ -21,7 +22,7 @@ class FiltersContainer extends Component {
     } else {
       return (
         <div className="row">
-          <div className="col-md-12">
+          <div className="col-md-12 FiltersContainerColumn">
             <FiltersButton handleChange={this.handleChange.bind(this, true)} />
           </div>
         </div>
