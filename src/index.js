9015b20f185c6fd12d4da9388cd783cc6f54846c
Adam Gray <adam@pateo.co>
2017-09-25 15:19:40 +0100

    Add skycabIT

--- src/index.js
+++ src/index.js
@@ -25,6 +25,11 @@ const store = createStore(reducer, {
         id: 2,
         label: 'HR',
         isChecked: false
+      },
+      {
+        id: 3,
+        label: 'IT',
+        isChecked: false
       }
     ]
   },
@@ -43,7 +48,7 @@ const store = createStore(reducer, {
           type: 'technology',
           login: 'https://monitor.zende.sk/accounts/478062/agents',
           docs: 'https://help.skycab.me/hc/en-gb/articles/115005060005',
-          tags: ['elephant', 'sandwich']
+          tags: []
         },
         {
           id: 2,
@@ -54,7 +59,33 @@ const store = createStore(reducer, {
           type: 'HR',
           login: 'https://monitor.zende.sk/accounts/873384/agents',
           docs: 'https://skycabhr.zendesk.com/hc/en-us/categories/200798885-Internal',
-          tags: ['monkey', 'television']
+          tags: []
+        },
+        {
+          id: 3,
+          name: 'Skycab IT',
+          color: '#ed725e',
+          url: 'https://skycabit.zendesk.com',
+          description: 'Internal ITSM instance operated by Skycab\'s IT department',
+          type: 'IT',
+          login: 'https://skycabit.zendesk.com/agent',
+          docs: 'https://drive.google.com/drive/folders/0B0QfIO5ptBCdQ2NPQXlsM2VCVms?usp=sharing',
+          tags: ['five most recent',
+            'announcements',
+            'jira',
+            'related ticket',
+            'notification app',
+            'oomnitza it asset management',
+            'conditional fields',
+            'proactive tickets',
+            'tymeshift',
+            'notification app',
+            'knowledge capture',
+            'interbot',
+            'calendar',
+            'myndbend process management light',
+            'calendar',
+            'ticket field manager']
         }
       ]
     }
